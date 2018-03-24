f1da35eaaa62cacd883a1b8c70c626477b433001
Alin Chindea <alin.chindea@gmail.com>
2018-03-24 15:40:00 +0000

    added javascript logic to open and close the burger menu

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-$(() => {
-  console.log('jQuery loaded');
+
+
+//with vanillaJS
+window.addEventListener('DOMContentLoaded', () => {
+
+  function classToggle() {
+    const navs = document.querySelectorAll('.navbar-items');
+
+    navs.forEach(nav => nav.classList.toggle('navbar-burgerToggle'));
+  }
+  const burgerMenu = document.querySelector('.navbar-burger');
+  burgerMenu.addEventListener('click', classToggle);
+
 });
+
+
+// $(() => {
+//   // console.log('jQuery loaded');
+//   // function classToggle() {
+//   //   const $navs = $('navbar-items');
+//   //   $navs.each(nav => nav.toggleClass('navbar-burgerOpen'));
+//   // }
+//
+//   $('.navbar-burger').on('click', function(){
+//     $('.navbar-burgerOpen').hide();
+//     $('.navbar-burgerOpen').toggle();
+//   });
+// });
